540969ab050435a54d89c8ae61ae5d06d7cfa7c4
Eric Figueiredo <eric.rq.figueiredo@gmail.com.br>
2024-12-11 13:20:42 -0300

    Atualizando dados do projeto em Angular 19

---
+++ src/app/components/moment-form/moment-form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-moment-form',
+  standalone: false,
+
+  templateUrl: './moment-form.component.html',
+  styleUrl: './moment-form.component.css'
+})
+export class MomentFormComponent implements OnInit {
+
+  constructor(){}
+
+  ngOnInit(): void {
+  }
+
+}
